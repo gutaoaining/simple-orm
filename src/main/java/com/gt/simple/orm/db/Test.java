ba1e4fff3f9bd22395514b432366e6fcf913a5c0
gutaoaining <1093656744@qq.com>
2023-02-25 22:16:08 +0800

    test commit

--- src/main/java/com/gt/simple/orm/db/Test.java
+++ src/main/java/com/gt/simple/orm/db/Test.java
@@ -3,5 +3,6 @@ package com.gt.simple.orm.db;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test contribute config again");
+        System.out.println("test contribute helloWorld");
     }
 }
